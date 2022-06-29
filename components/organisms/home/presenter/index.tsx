ada18480b81a7b03d8da831a272b465ae494f824
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-06-29 22:43:01 +0900

    fix: 🐛 style

--- components/organisms/home/presenter/index.tsx
+++ components/organisms/home/presenter/index.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
-import { Box, Container, Heading, Image, useColorModeValue } from '@chakra-ui/react';
+import { Box, Container, Heading, Image } from '@chakra-ui/react';
 
 interface HomePresenterProps {
-  profileImg: string
+  profileImg: string;
 }
 
 /**
@@ -12,22 +12,19 @@ interface HomePresenterProps {
 export const HomePresenter: FC<HomePresenterProps> = ({ profileImg }) => {
   return (
     <Container>
-      <Box borderRadius={'lg'} bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align={'center'}>
-        Thanks for visiting. I&apos;m working as a full-stack developer.
-      </Box>
-      <Box display={{ md: 'flex' }}>
+      <Box display={{ md: 'flex' }} my={100}>
         <Box flexGrow={1}>
-          <Heading as={'h2'} variant={'page-title'}>
-            Lil shimon
+          <Heading as={'h2'} variant={'page-title'} mb={10}>
+            Kenta Shimosawa,
           </Heading>
           <p>
-            SoftWare directer in 3 months of graduate (Front-End / Server-Side / Mobile)
+            Japan born, Saitama based Product manager, & developer.
           </p>
         </Box>
 
         <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align={'center'}>
-          <Image borderColor={'whiteAlpha.800'} borderWidth={2} borderStyle={'solid'} maxWidth={'100px'}
-                 display={'inline-block'} borderRadius={'full'} src={profileImg} alt={'Profile Image'} />
+          <Image borderColor={'whiteAlpha.800'} borderWidth={2} borderStyle={'solid'} maxWidth={'350px'}
+                 display={'inline-block'} src={profileImg} alt={'Profile Image'} />
         </Box>
       </Box>
     </Container>
